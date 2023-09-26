28801d58bb784a201318b4b802c3ee7fbd66df55
sujithaswami <144520487+sujithaswami@users.noreply.github.com>
2023-09-26 16:31:23 +0530

    Add files via upload

---
+++ stringtransform.py
@@ -0,0 +1,78 @@
+#captalize
+a = "hello sujitha"
+print (a.capitalize())
+#title
+s = "python full stack"
+print(s.title())
+#Upper case
+k = "sujitha swami"
+print(k.upper())
+#lower case
+f = "SUJITHA"
+print(f.lower())
+#case fold
+s = "CEMETRY"
+print(s.casefold())
+#swap case
+k = "Hello World"
+print(k. swapcase())
+#string check functions
+#isnumeric
+a = "1223"
+print(a.isnumeric())
+d = "sujitha123"
+print(d.isnumeric())
+#isalnum
+s = "suji123"
+print(s.isalnum())
+a = "@"
+print(a.isalnum())
+#isdecimal
+k = "25"
+print(k.isdecimal())
+k = "suji12.3"
+print(k.isdecimal())
+#isdigit
+s = "123"
+print(s.isdigit())
+a = "suji12.83"
+print(a.isdigit())
+#isasscii
+a = "sujitha"
+print(a.isascii())
+s = "UTF-8"
+print(s.isascii())
+#isupper
+d ="sujitha"
+print(d.isupper())
+s ="SUJITHA"
+print(s.isupper())
+#islower
+s = "SUJItha"
+print(s.islower())
+d = "sujitha"
+print(d.islower())
+#isspace
+d = "   "
+print(d.isspace())
+d = " @"
+print(d.isspace())
+#isidentifier
+f = "sujitha"
+print(f.isidentifier())
+p = "123suji"
+print(p.isidentifier())
+p = "123"
+print(p.isidentifier())
+l = "@"
+print(l.isidentifier())
+#printabel
+g = "sujitha"
+print(g.isprintable())
+h = " "
+print(g.isprintable())
+#istitle
+s ="suji hi"
+print(s.istitle())
+k = "Suji"
+print(k.istitle())
